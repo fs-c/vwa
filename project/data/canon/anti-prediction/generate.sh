53441d2ba26f12d96d3f1980eaf63b0769fdde32
fsoc <root@fsoc.space>
2021-06-27 19:02:36 +0200

    Push data

---
+++ project/data/canon/anti-prediction/generate.sh
@@ -0,0 +1,6 @@
+../../../build/benchmark -r -a 16 -t linear -s 16 -c 16 -o ./linear/xs       # 2^4
+../../../build/benchmark -r -a 16 -t linear -s 256 -c 128 -o ./linear/sm      # 2^8
+../../../build/benchmark -r -a 16 -t linear -s 262144 -c 128 -o ./linear/md   # 2^18
+
+../../../build/benchmark -r -a 16 -t quadratic -s 262144 -c 128 -o ./quadratic/coarse
+../../../build/benchmark -r -a 16 -t quadratic -s 262144 -c 512 -o ./quadratic/fine
